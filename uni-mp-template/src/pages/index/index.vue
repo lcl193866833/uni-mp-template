83cd66dd818ff3b85d4887410e7f32d22d01b373
lcl <193866833@qq.com>
2023-11-07 16:06:28 +0800

    login

---
+++ uni-mp-template/src/pages/index/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <view class="index">index</view>
+  <div class="ellipsis">哪里贵了? 这么多年都是这个价格</div>
+  <div class="flex justify-center h-100 items-center">
+    <div class="w-10 h-10 bg-pink-500"></div>
+  </div>
+</template>
+
+<style lang="scss">
+//
+</style>
