63faa510c510f7f1841d23dd6680331fd976cef4
lcl <193866833@qq.com>
2023-11-18 20:27:57 +0800

    商品模块

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,58 +1,97 @@
 <template>
-  <view class="navbar" :style="{ paddingTop: safeAreaInsets?.top + 'px' }">
-    <!-- logo文字 -->
-    <view class="flex items-center h-64 pl-30 pt-20">
-      <image class="w-200 h-50" src="@/static/images/logo.png"></image>
-      <text class="logo-text">新鲜 · 亲民 · 快捷</text>
-    </view>
-    <!-- 搜索条 -->
-    <view class="search">
-      <text class="icon-search">搜索商品</text>
-      <text class="icon-scan"></text>
-    </view>
+  <view class="viewport">
+    <!-- 自定义导航栏 -->
+    <CustomNavbar></CustomNavbar>
+    <!-- 滚动容器 -->
+    <scroll-view
+      refresher-enabled
+      @refresherrefresh="onRefresherrefresh"
+      :refresher-triggered="isTriggered"
+      scroll-y
+      @scrolltolower="onScrolltolower"
+      class="scroll-view"
+    >
+      <wSwiper :list="bannerList"></wSwiper>
+      <!-- 商品分类 -->
+      <CategoryPanel :list="categoryList" />
+      <!-- 热门推荐 -->
+      <HotPannel :list="hotList" />
+      <!-- 猜你喜欢 -->
+      <Guess ref="guessRef" />
+      <!--  -->
+    </scroll-view>
   </view>
 </template>
+<script lang="ts" setup>
+// @ts-nocheck
+import CustomNavbar from './Components/CustomNavbar'
+import CategoryPanel from './Components/CategoryPanel'
+import HotPannel from './Components/HotPannel'
+import type { GuessInstance } from '@/types/Components'
+import Guess from '@/components/Guess/Guess.vue'
 
-<script setup lang="ts">
-// 获取屏幕边界到安全区域距离
-const { safeAreaInsets } = uni.getSystemInfoSync()
+// 获取轮播图
+const bannerList = ref<BannerItem[]>([])
+
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+}
+
+// 获取分类数据
+const categoryList = ref<CategoryItem[]>([])
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  categoryList.value = res.result
+}
+// 获取热门推荐数据
+const hotList = ref([])
+const getHomeHotData = async () => {
+  const res = await getHomeHotAPI()
+  hotList.value = res.result
+}
+
+// 是否加载中标记
+const isLoading = ref(false)
+onLoad(async () => {
+  isLoading.value = true
+  await Promise.all([getHomeBannerData(), getHomeCategoryData(), getHomeHotData()])
+  isLoading.value = false
+})
+
+// 获取猜你喜欢组件实例
+const guessRef = ref<GuessInstance>()
+// 滚动触底事件
+const onScrolltolower = () => {
+  guessRef.value?.getMore()
+}
+
+// 下拉刷新状态
+const isTriggered = ref(false)
+// 自定义下拉刷新被触发
+const onRefresherrefresh = async () => {
+  // 开启动画
+  isTriggered.value = true
+  // 重置猜你喜欢组件数据
+  guessRef.value?.resetData() // 加载数据
+  await Promise.all([getHomeBannerData(), getHomeCategoryData(), getHomeHotData()]) // 关闭动画
+  isTriggered.value = false
+}
 </script>
-
-<style lang="scss" scoped>
-/* 自定义导航条 */
-.navbar {
-  @apply flex flex-col pt-20;
-  background-image: url(@/static/images/navigator_bg.png);
-  background-size: cover;
-  .logo-text {
-    flex: 1;
-    line-height: 28rpx;
-    color: #fff;
-    margin: 2rpx 0 0 20rpx;
-    padding-left: 20rpx;
-    border-left: 1rpx solid #fff;
-    font-size: 26rpx;
-  }
-  .search {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 0 10rpx 0 26rpx;
-    height: 64rpx;
-    margin: 16rpx 20rpx;
-    color: #fff;
-    font-size: 28rpx;
-    border-radius: 32rpx;
-    background-color: rgba(255, 255, 255, 0.5);
-  }
-  .icon-search {
-    &::before {
-      margin-right: 10rpx;
-    }
-  }
-  .icon-scan {
-    font-size: 30rpx;
-    padding: 15rpx;
-  }
+<style lang="scss">
+page {
+  background-color: #f7f7f7;
+  height: 100%;
+  overflow: hidden;
+}
+/* 设置滚动区高度 */
+.viewport {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.scroll-view {
+  flex: 1;
+  overflow: hidden;
 }
 </style>
